cb00bd72553f45d694f9fc2f0796a72f9e6028a9
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-02-15 20:56:23 -0800

    update

--- code/config/locomotion_config.py
+++ code/config/locomotion_config.py
@@ -6,7 +6,7 @@ class Config(ParamsProto):
     # misc
     seed = 100
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    bucket = '/home/aajay/weights/'
+    bucket = '/iliad/u/manasis/decision-diffuser-updated/code/weights/'
     dataset = 'hopper-medium-expert-v2'
 
     ## model
