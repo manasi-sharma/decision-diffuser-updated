a2921ca37292445fef55b6b67c88e680482350c5
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-02-28 01:06:15 -0800

    update

--- code/diffuser/utils/training.py
+++ code/diffuser/utils/training.py
@@ -181,9 +181,9 @@ class Trainer(object):
         os.makedirs(savepath, exist_ok=True)
         # logger.save_torch(data, savepath)
         if self.save_checkpoints:
-            savepath = os.path.join(savepath, f'state_{self.step}.pt')
+            savepath = os.path.join(savepath, f'state_attn_{self.step}.pt')
         else:
-            savepath = os.path.join(savepath, 'state.pt')
+            savepath = os.path.join(savepath, 'state_attn.pt')
         torch.save(data, savepath)
         logger.print(f'[ utils/training ] Saved model to {savepath}')
 
@@ -191,7 +191,7 @@ class Trainer(object):
         '''
             loads model and ema from disk
         '''
-        loadpath = os.path.join(self.bucket, logger.prefix, f'checkpoint/state.pt')
+        loadpath = os.path.join(self.bucket, logger.prefix, f'checkpoint/state_attn.pt')
         # data = logger.load_torch(loadpath)
         data = torch.load(loadpath)
 
